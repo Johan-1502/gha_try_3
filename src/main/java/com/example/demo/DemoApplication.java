9ba0bcc875b38717b57be31ac5600a9fa06773df
JohanGil1502 <johan.gil@uptc.edu.co>
2025-03-15 16:02:28 -0500

    feature: atributo agregado

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	private int id;
+	private int id2;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
