5eaf2846e763c52b5f89ec0f77795ef88c803b63
JohanGil1502 <johan.gil@uptc.edu.co>
2025-03-15 16:08:40 -0500

    feat: atributo agregado

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	private int id;
 	private int id2;
 	private int id3;
+	private int id4;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
